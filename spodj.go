83102fc55eb5213aea40a88ca9341869996c82f8
Mike Hughes <intermernet@gmail.com>
2021-11-12 18:09:37 +1100

    Cleanup error values and remove fatal exits

--- spodj.go
+++ spodj.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -27,6 +28,12 @@ var (
 	scope   = []string{spotifyauth.ScopeUserReadPrivate, spotifyauth.ScopePlaylistModifyPrivate, spotifyauth.ScopePlaylistReadPrivate}
 	auth    *spotifyauth.Authenticator
 	clMap   = new(ClientMap)
+
+	errDecode   = errors.New("could not decode JSON")
+	errToken    = errors.New("could not get token")
+	errState    = errors.New("state mismatch")
+	errRecomm   = errors.New("could not get recommendations")
+	errPlaylist = errors.New("could not create playlist")
 )
 
 func init() {
@@ -108,8 +115,8 @@ func doAPI(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(c.a)
 	if err != nil {
-		http.Error(w, "could not decode JSON", http.StatusInternalServerError)
-		log.Printf("could not decode JSON. %s", err)
+		http.Error(w, errDecode.Error(), http.StatusInternalServerError)
+		log.Printf("%s: %s", errDecode, err)
 	}
 	clMap.Set(c.state, c)
 	url := auth.AuthURL(c.state)
@@ -120,12 +127,12 @@ func completeAuth(w http.ResponseWriter, r *http.Request) {
 	c := clMap.Get(r.FormValue("state"))
 	tok, err := auth.Token(r.Context(), c.state, r)
 	if err != nil {
-		http.Error(w, "could not get token", http.StatusInternalServerError)
-		log.Printf("could not get token %s\n", err)
+		http.Error(w, errToken.Error(), http.StatusInternalServerError)
+		log.Printf("%s: %s\n", errToken, err)
 	}
 	if st := r.FormValue("state"); st != c.state {
-		http.NotFound(w, r)
-		log.Fatalf("state mismatch: %s != %s\n", st, c.state)
+		http.Error(w, errState.Error(), http.StatusInternalServerError)
+		log.Printf("%s: %s != %s\n", errState, st, c.state)
 	}
 	cl := spotify.New(auth.Client(r.Context(), tok))
 	c = Client{
@@ -135,13 +142,13 @@ func completeAuth(w http.ResponseWriter, r *http.Request) {
 	}
 	pl, err := c.getRecs(r.Context(), c.a)
 	if err != nil {
-		http.Error(w, "could not get recommendations", http.StatusInternalServerError)
-		log.Fatalf("could not get recommendations %s", err)
+		http.Error(w, errRecomm.Error(), http.StatusInternalServerError)
+		log.Printf("%s: %s", errRecomm, err)
 	}
 	plURL, err := c.createPlaylist(r.Context(), pl, c.a.Name)
 	if err != nil {
-		http.Error(w, "could not create playlist", http.StatusInternalServerError)
-		log.Printf("could not create playlist %s", err)
+		http.Error(w, errPlaylist.Error(), http.StatusInternalServerError)
+		log.Printf("%s: %s", errPlaylist, err)
 	}
 	log.Printf("%s\n", plURL)
 	clMap.Delete(c.state)
